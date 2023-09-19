bd349a3f9a6c653e384e9b6283eacb2b078719bc
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-09-18 23:46:58 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,24 +5,16 @@ let numberList = []
 function finalizar() {
     const inAll = numberList.length
 
-    if(inAll == 0){
+    if (inAll == 0) {
         alert('add one value!')
-    }else{
-        let msgRegister = document.getElementById('msg-register')
-        let msgBigger = document.getElementById('msg-large')
-        let msgSmaller = document.getElementById('msg-small')
-        let msgSum = document.getElementById('msg-sum')
-        let msgAverage = document.getElementById('msg-average')
-
+    } else {
         msglist.classList.replace('off','on')
 
-        
+        document.getElementById('msg-register').innerHTML = `<p>In all,we have <strong>${inAll}</strong> numbers registered.</p>`
 
-        msgRegister.innerHTML = `<p>In all,we have <strong>${inAll}</strong> numbers registered.</p>`
+        document.getElementById('msg-large').innerHTML = `<p>The bigger value informed he was: <strong>${numberList[inAll-1]}</strong></p>`
 
-        msgBigger.innerHTML = `<p>The bigger value informed he was: <strong>${numberList[inAll-1]}</strong></p>`
-
-        msgSmaller.innerHTML = `<p>The smaller value informed is: <strong>${numberList[0]}</strong></p>`
+        document.getElementById('msg-small').innerHTML = `<p>The smaller value informed is: <strong>${numberList[0]}</strong></p>`
 
 
         let sum = 0
@@ -30,11 +22,11 @@ function finalizar() {
 
             sum += numberList[cont]
 
-            if (cont == (inAll-1)){
-                msgSum.innerHTML = `<p>adding alls at the values we have: <strong>${sum}</strong></p>`
+            if (cont == (inAll-1)) {
+                document.getElementById('msg-sum').innerHTML = `<p>adding alls at the values we have: <strong>${sum}</strong></p>`
 
                 let average = sum/inAll
-                msgAverage.innerHTML = `<p>The average at the values typed is: <strong>${average.toFixed(1)}</strong></p>`
+                document.getElementById('msg-average').innerHTML = `<p>The average at the values typed is: <strong>${average.toFixed(1)}</strong></p>`
             }
         }}
 }
@@ -45,24 +37,24 @@ function toAdd() {
     const number = document.getElementById('inumber')
     const numberValue = (number.value)
 
-    if (numberValue == ''){
+    if (numberValue == '') {
         alert('ERRO, box empty!')
 
-    }else{
+    } else {
         const numberValue = Number(number.value)
 
-        if(numberValue < 1 || numberValue > 100){
+        if (numberValue < 1 || numberValue > 100) {
             alert('ERRO type it a number in between 1 and 100')
 
-        }else{
+        } else {
             
             const verification = numberList.indexOf(numberValue)
             number.value = ''
             number.focus()
 
-            if (verification != -1){
+            if (verification != -1) {
                 alert`Number ${numberValue} exist at on list or invalid!`
-            }else{
+            } else {
                 numberList.push(numberValue)
                 numberList.sort((a,b) => a - b);
                 
